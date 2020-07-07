e68c48789e03454c6fa76c5458808d17b7c5c118
Brian Shacklett <brian.shacklett@slalom.com>
2020-07-07 12:12:57 -0400

    Enable case-insensitive completions

--- conf.d/99-options.zsh
+++ conf.d/99-options.zsh
@@ -4,3 +4,6 @@ export EDITOR=vim
 # Don't show AWS prompt from plugin
 export SHOW_AWS_PROMPT=false
 export SPACESHIP_GCLOUD_SHOW=false
+
+# Enable case-insensitive completions
+zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*'
