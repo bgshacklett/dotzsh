a6241a60760e05981640386523c82ce3e25cbfd0
Brian Shacklett <brian.shacklett@slalom.com>
2020-08-04 15:22:05 -0400

    Use OMZ fzf tab completion

--- conf.d/90-fzf.zsh
+++ conf.d/90-fzf.zsh
@@ -3,6 +3,10 @@
 ## Install fzf binary
 zinit ice from"gh-r" as"program"
 zinit light junegunn/fzf-bin 
+# Note: Following files must be downloaded manually (for now):
+# https://raw.githubusercontent.com/junegunn/fzf/master/shell/completion.zsh
+# https://raw.githubusercontent.com/junegunn/fzf/master/shell/key-bindings.zsh
 
 ## Enable Tab Completion with fzf
-zinit light Aloxaf/fzf-tab
+export FZF_BASE="$(dirname $(which fzf))"
+zinit wait lucid for OMZ::plugins/fzf
