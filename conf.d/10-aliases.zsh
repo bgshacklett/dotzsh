57412b74226ff4f302740de8dcf7c03bc8dc0e26
Brian Shacklett <brian.shacklett@slalom.com>
2022-08-23 14:14:51 -0400

    Add aliases

---
+++ conf.d/10-aliases.zsh
@@ -0,0 +1,13 @@
+alias ll="ls -lh"
+alias k=kubectl
+alias vi=nvim
+
+# Accounting for PowerShell muscle memory
+function ri() { printf "\a"; echo "wrong command" >&2; rm $@ }
+alias ri=ri
+
+alias rehist="fc -RI"
+alias groovysh="groovysh -Djava.awt.headless=true"
+alias git-nwadd="git diff -U0 -w -b --ignore-blank-lines --no-color | git apply --ignore-whitespace --unidiff-zero -"
+alias isodate="date +"%Y-%m-%dT%H%M%S%z""
+alias zisodate="date -u +"%Y-%m-%dT%H%M%SZ""
